c40fdf8f5b74b0c4d81bdaf2ec63adbf9770e49a
MiguelVaDu <angel301001_vd@unmsm.edu.pe>
2024-06-25 00:22:32 -0500

    update services

--- schemas/respuesta_schema.py
+++ schemas/respuesta_schema.py
@@ -1,8 +1,9 @@
-from utils.ma import ma
-from models.respuesta import Respuesta
-from schemas.test_schema import TestSchema
-from schemas.pregunta_schema import PreguntaSchema
 from schemas.alternativa_schema import AlternativaSchema
+from schemas.pregunta_schema import PreguntaSchema
+from schemas.test_schema import TestSchema
+from models.respuesta import Respuesta
+from utils.ma import ma
+
 
 class RespuestaSchema(ma.Schema):
   class Meta:
@@ -18,6 +19,7 @@ class RespuestaSchema(ma.Schema):
     )
     
   test = ma.Nested(TestSchema)
+  pregunta = ma.Nested(PreguntaSchema)
   alternativa = ma.Nested(AlternativaSchema)
 
 respuesta_schema = RespuestaSchema()
